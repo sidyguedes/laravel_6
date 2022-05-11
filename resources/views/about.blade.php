0e8fa6dd40b49b1b75e01cb1758e59ee9f79b732
Sidy Guedes <sidy_guedes@econet.matriz.org>
2022-05-11 13:29:37 -0300

    testando rotas

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+
+OLA PESSOAL
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/about', function(){
+    return view('about');
+});
+
+Route::get('/contato', function(){
+    return 'ola';
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
